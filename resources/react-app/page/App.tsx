b25c5ab1868cac83fcf143636a392a7df3c1127c
yannickfogang <fogang24@gmail.com>
2022-07-09 21:38:43 +0200

    update migration user

--- resources/react-app/page/App.tsx
+++ resources/react-app/page/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export const App :  React.FC = (): JSX.Element => {
-    return <div className="font-bold text-3xl">Hello Word</div>
+    return (
+        <div className="flex justify-center">
+            <div className="mt-8">
+                <h1 className="text-gray-800 text-3xl font-bold">Levantou Scrapper</h1>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
 }
